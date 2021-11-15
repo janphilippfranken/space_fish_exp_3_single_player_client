c6e705af5fe45c088c089485291577a4923b70b8
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2021-11-15 22:24:51 +0000

    updates

--- src/socket.io/URLs.js
+++ src/socket.io/URLs.js
@@ -1,7 +1,8 @@
 class URLs {
     static SERVER_DEV = 'http://localhost:5000'; 
     static SERVER_DEPLOY = 'https://space-fish.herokuapp.com';
-    static SERVER_ECO = 'https://eco.ppls.ed.ac.uk';
+    // static SERVER_ECO = 'https://eco.ppls.ed.ac.uk';
+    static SERVER_ECO = 'http://localhost:5000'
 }
 
 export default URLs;
