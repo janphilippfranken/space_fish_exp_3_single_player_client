1968ab976eca3e4dcff6855934449e34b1a9dd29
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2021-12-02 13:30:02 +0000

    updates

--- src/components/PlanetFrame/PlanetFrame.js
+++ src/components/PlanetFrame/PlanetFrame.js
@@ -502,14 +502,14 @@ const PlanetFrame = props => {
         setSimulatedResponse(false);
         if (checkScoreDisplays(9) == true) {
             // if condition is structure known:
-            dispatch(goToPage(pages.DEBRIEF));
+            // dispatch(goToPage(pages.DEBRIEF));
             // if not known activate all of these again!
-            // setShowStructure(true);
-            // setRemainingTime(120);
-            // setShowPrivate(false);
-            // setBeforeSelect('none');
-            // setAfterSelect('none');
-            // structureDelayFunc();
+            setShowStructure(true);
+            setRemainingTime(120);
+            setShowPrivate(false);
+            setBeforeSelect('none');
+            setAfterSelect('none');
+            structureDelayFunc();
             // console.log('h');
             return false;
         }
